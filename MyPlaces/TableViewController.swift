702421f8f0dacf8dbadafec6a84eb4901d0b15c8
Nurqalam <tnurkalam@gmail.com>
2022-03-04 14:22:47 +0500

    Custom Table View Cell

---
+++ MyPlaces/TableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Nurqalam on 24.02.2022.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    private var restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy"
+    ]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.nameLabel.text = restaurantNames[indexPath.row]
+        cell.imageOfPlace.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
+        cell.imageOfPlace.clipsToBounds = true
+        return cell
+    }
+
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 95
+    }
+}
+
